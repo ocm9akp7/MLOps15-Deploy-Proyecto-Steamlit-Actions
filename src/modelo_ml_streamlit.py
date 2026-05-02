541544340d74ec9d0c48abb99640a92bfd598404
Obed Castillo <obed.castillom@gmail.com>
2026-05-02 11:14:52 -0600

    Proyecto-Streamlit-v2

--- src/modelo_ml_streamlit.py
+++ src/modelo_ml_streamlit.py
@@ -46,7 +46,7 @@ def prediccion_o_inferencia(pipeline_de_test, datos_de_test):
 #Diseno de la Interface
 st.title("Proyecto Modelo ML - Nombre Apellido - DATAPATH")
 
-image = Image.open('images/datapath-logo.png') #COMPLETAR CON UNA IMAGEN
+image = Image.open('src/images/datapath-logo.png') #COMPLETAR CON UNA IMAGEN
 st.image(image, use_container_width=True) #use_column_width esta "deprecated"
 
 st.sidebar.write("Suba el archivo CSV correspondiente para realizar la predicción")
@@ -64,7 +64,7 @@ if uploaded_file is not None:
     st.dataframe(df_de_los_datos_subidos)
 #-------------------------------------------------------------------------------------------
 #Cargar el Modelo ML o Cargar el Pipeline
-pipeline_de_produccion = joblib.load('precio_casas_pipeline.joblib')
+pipeline_de_produccion = joblib.load('src/precio_casas_pipeline.joblib')
 
 if st.sidebar.button("click aqui para enviar el CSV al Pipeline"):
     if uploaded_file is None:
